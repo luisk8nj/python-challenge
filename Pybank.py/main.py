8b73911bf8d6f644a909d93fd2b9dc5a59e385f1
luisk8nj <luisk8nj@gmail.com>
2017-09-07 02:14:32 -0400

    update

--- Pybank.py/main.py
+++ Pybank.py/main.py
@@ -1,25 +1,70 @@
-# First we'll import the os module 
-# This will allow us to create file paths across operating systems
-import os
+# Import ependencies
 import csv
-#import pandas as pd
 
-csvpath = os.path.join('budget_data_1.csv')
+# Company csv file to read load 
+file_to_load = "budget_data_1.csv"
 
-# Improved Reading using CSV module
-with open(csvpath) as csvfile:
+#Print output results to pdf file 
+file_to_output = "budget_analysis_1.pdf"
 
-    # CSV reader specifies delimiter and variable that holds contents
-    csvreader = csv.reader(csvfile, delimiter=',')
-    num_cols = 0
-    #  Each row is read as a row
-    for row in csvreader:
-        # print(row)
-        num_cols = num_cols + 1
-        print(num_cols)
-
-        
+# Track various revenue parameters
+# The total number of months included in the dataset
+# The total amount of revenue gained over the entire period
+# The average change in revenue between months over the entire period
+# The greatest increase in revenue (date and amount) over the entire period
+# The greatest decrease in revenue (date and amount) over the entire period
 
 
+total_months = 0
+prev_revenue = 0
+month_of_change = []
+revenue_change_list = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999]
+total_revenue = 0
 
+# Read the csv and convert it into a list of dictionaries
+with open(file_to_load) as revenue_data:
+    reader = csv.DictReader(revenue_data)
 
+    for row in reader:
+
+        # Track the total
+        total_months = total_months + 1
+        total_revenue = total_revenue + int(row["Revenue"])
+
+        # Track the revenue change
+        revenue_change = int(row["Revenue"]) - prev_revenue
+        prev_revenue = int(row["Revenue"])
+        revenue_change_list = revenue_change_list + [revenue_change]
+        month_of_change = month_of_change + [row["Date"]]
+
+        # Calculate the greatest increase
+        if (revenue_change > greatest_increase[1]):
+            greatest_increase[0] = row["Date"]
+            greatest_increase[1] = revenue_change
+
+        # Calculate the greatest decrease
+        if (revenue_change < greatest_decrease[1]):
+            greatest_decrease[0] = row["Date"]
+            greatest_decrease[1] = revenue_change
+
+# Calculate the Average Revenue Change
+revenue_avg = sum(revenue_change_list) / len(revenue_change_list)
+
+# Generate Output Summary
+output = (
+    f"\nFinancial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total Revenue: ${total_revenue}\n"
+    f"Average Revenue Change: ${revenue_avg}\n"
+    f"Greatest Increase in Revenue: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease in Revenue: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+
+# Print the output (to integrated terminal)
+print(output)
+
+# Export the results to pdf file
+with open(file_to_output, "w") as pdf_file:
+    pdf_file.write(output)
